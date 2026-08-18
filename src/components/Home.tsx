5c937143bee65558594a971053ea50626ebdf81e
Christian Irvine <christian.irvine04@gmail.com>
2026-08-18 14:32:53 +1200

    feat: style headers and page to be dark

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,9 @@ import '../App.css'
 
 const Home: React.FC =() => {
   return (
-    <>
+    <div>
       <p>Hey this is my portfolio and I am Christian Irvine!</p>
-    </>
+    </div>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -24,13 +24,13 @@ const NavBar: React.FC = () => {
 
   return (
     <>
-      <div className="bg-slate-700 flex justify-end w-full">
+      <div className="flex justify-end w-full navbar">
         <Link className="mr-auto px-5" to={`/`}>
           <h1>Christian</h1>
         </Link>
         {routes.map((route: NavPair) => (
           <Link to={`/${route.key}`} key={route.key}>
-            <div className="w-50 py-8 hover:bg-slate-800">
+            <div className="w-50 py-8 navbar-button">
               <h3>{route.name}</h3>
             </div>
           </Link>
