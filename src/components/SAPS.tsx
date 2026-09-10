3f43be51bc322b54ddadc8a3aaae18487238a31a
Christian Irvine <christian.irvine04@gmail.com>
2026-09-10 13:13:36 +1200

    feat: add content to saps page

--- src/components/SAPS.tsx
+++ src/components/SAPS.tsx
@@ -12,6 +12,18 @@ const SAPS: React.FC =() => {
     <>
       <h1 className="p-5">S.A.P.S</h1> 
       <Carousel contents={carouselContents}/>
+
+      <div className="flex justify-center">
+        <div className="w-2/3 pt-15 pb-15 text-left leading-8">
+          <p>
+            Saps is an action steal VR game developed by me and a few other students as our capstone project at Otago Polytechnic.
+            The goal of the game is to sneak around and find a target with specific information given to you on a card.
+            Once you have found your guy you have to assassinate him stealthily without getting caught.
+            However, if you get detected it isn't over yet as you can still escape!
+          </p>
+        </div>
+
+      </div>
     </>
   )
 }
